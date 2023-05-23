327712bb1c8e386bda29d891e0f01a2b2c9df30e
Wenya Teixeira <wenyatexprojetos@gmail.com>
2023-05-22 21:30:18 -0300

    Aulas 16, 17, 18 - Loops while, for, break, continue

---
+++ com/loaiane/cursojava/licaodecasa/aula18/BreakEContinue.java
@@ -0,0 +1,37 @@
+package com.loaiane.cursojava.licaodecasa.aula18;
+
+import java.util.Scanner;
+
+public class BreakEContinue {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		/*
+		 *encontra os multiplos de 7
+		 * System.out.println("Entre com um numero"); int num = s.nextInt();
+		 * 
+		 * System.out.println("Entre com um limite"); int max = s.nextInt();
+		 * 
+		 * for (int i = num; i <= max; i++) { System.out.println(i); if(i % 7 ==0) {
+		 * System.out.println("O valor de i = " + i); break; } }
+		 */
+
+		System.out.println("Entre com um numero");
+		int num = s.nextInt();
+
+		System.out.println("Entre com um limite");
+		int max = s.nextInt();
+        //pula os multiplos de 7
+		for (int i = num; i <= max; i++) {
+			if (i % 7 == 0) {
+				continue;
+			}
+			System.out.println("O valor de i = " + i);
+			;
+		}
+
+	}
+
+}
